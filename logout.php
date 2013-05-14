45c501ecb25a6aae9af73a360d5ed1507abaad73
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2013-05-14 23:18:29 +0300

    fixed bug

--- logout.php
+++ logout.php
@@ -3,8 +3,10 @@
 // ionutvmi@gmail.com 
 // Sep 2012
 // master-land.net
+include "inc/settings.php";
 session_start();
-session_destroy();
+session_unset("adminpass");
+$path_info = parse_url($set->url);
 setcookie("pass", 0, time() - 3600 * 24 * 30, $path_info['path']); // delete
 
 header("Location: ".$_SERVER["HTTP_REFERER"]);
